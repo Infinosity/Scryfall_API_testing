5c5b2e2b8bb8569c7642544498829561079b03ec
infinosity <infinosity@protonmail.com>
2025-12-19 21:01:15 -0600

    finished card image fetching, needs styling and other formatting

--- mtg_card_search/src/api_intf.rs
+++ mtg_card_search/src/api_intf.rs
@@ -1,10 +1,14 @@
 use serde::{Deserialize, Serialize};
 use reqwest::Client;
-use reqwest::Error as reqwest_Error;
-use std::error::Error;
 use url::form_urlencoded::{byte_serialize};
+use anyhow::Result;
+use iced::widget::image::Handle;
+use std::sync::Arc;
 
-#[derive(Debug, Serialize, Deserialize, Default)]
+// Required for Scryfall API
+static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+
+#[derive(Debug, Serialize, Deserialize, Default, Clone)]
 pub struct Card {
   pub name: String,
   #[serde(rename = "mana_cost")]
@@ -16,4 +20,69 @@ pub struct Card {
   pub oracle_text: String,
   pub power: Option<String>,
   pub toughness: Option<String>,
+  #[serde(default)]
+  pub image_uris: ImageUris,
+  #[serde(skip)]
+  pub image_handle: Option<Handle>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+pub struct ImageUris {
+  #[serde(rename = "png")]
+  pub png: Option<String>,
+}
+
+// Entry point
+pub async fn fetch_card (card_name: String) -> Result<Card> {
+  let client = build_client()?;
+
+  let url = get_url(&card_name)?;
+
+  // Perform the request
+  let response = client
+      .get(url)
+      .send()
+      .await?;
+
+  // Print info
+  println!("Status: {}", response.status());
+  let json= response.text().await?;
+  let mut card: Card = serde_json::from_str(&json)?;
+  if let Some(url) = card.image_uris.png.clone() {
+    match download_image_handle(client.clone(), url).await {
+      Ok(handle) => {
+        card.image_handle = Some(handle);
+        println!("Downloaded PNG image!");
+      }
+      Err(err) => {
+        eprintln!("Failed to download PNG image: {}", err);
+      }
+    }
+  }
+  else {
+    println!("Card has no PNG image URL");
+  }
+
+
+  Ok(card)
+}
+
+async fn download_image_handle(client: Arc<Client>, url: String) -> Result<Handle> {
+  let response = client.get(&url).send().await?;
+  let bytes = response.bytes().await?;
+  Ok(Handle::from_bytes(bytes))
+}
+
+// Build the client
+fn build_client() -> Result<Arc<Client>> {
+    let client = reqwest::Client::builder()
+      .user_agent(APP_USER_AGENT)
+      .build()?;
+    Ok(Arc::new(client))
+}
+
+fn get_url(card_name: &str) -> Result<String> {
+
+  let card_encoded: String = byte_serialize(card_name.trim().as_bytes()).collect();
+  Ok(format!("https://api.scryfall.com/cards/named?exact={card_encoded}"))  
 }
--- mtg_card_search/src/main.rs
+++ mtg_card_search/src/main.rs
@@ -1,5 +1,6 @@
-use iced::{Element, Theme, Renderer};
-use iced::widget::{button, column, text, text_input, Column};
+use iced::{Element, Theme, Renderer, Task, Length};
+use iced::widget::{Image, button, column, text, text_input, image::Handle};
+use anyhow::Result;
 
 mod api_intf;
 
@@ -11,32 +12,60 @@ fn main() -> iced::Result {
 #[derive(Debug, Default)]
 struct State {
     user_input: String,
-    //fetched_data: api_intf::Card,
-    fetched_data: String,
-    loading: bool,
+    card_data: api_intf::Card,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     Fetch,
     InputChanged(String),
+    CardLoaded(Result<api_intf::Card, String>),
 }
 
 impl State {
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::Fetch => {
-                self.fetched_data = self.user_input.clone();
+                let card_name_clone = self.user_input.clone();
+                Task::perform(
+                    async move {
+                        match api_intf::fetch_card(card_name_clone).await {
+                            Ok(card) => Ok(card),
+                            Err(err) => Err(err.to_string()),
+                        }
+                    },
+                    Message::CardLoaded,
+                )
             }
             Message::InputChanged(user_input) => {
                 self.user_input = user_input;
+                Task::none()
+            }
+            Message::CardLoaded(Ok(card)) => {
+                self.card_data = card;
+                Task::none()
+            }
+            Message::CardLoaded(Err(err)) => {
+                eprintln!("Failed to load card: {}", err);
+                Task::none()
             }
         }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        column![
-            text(&self.fetched_data).size(50),
+        let image_or_placeholder: Element<'_, Message> = match &self.card_data.image_handle {
+            Some(handle) => Image::<Handle>::new(handle.clone())
+                .width(Length::Fixed(500.0))
+                .height(Length::Fixed(700.0))
+                .into(),
+            None => text("No Image")
+                .size(20)
+                .into(),
+        };
+        column![   
+            image_or_placeholder,         
+            text(&self.card_data.name).size(30),
+            text(&self.card_data.oracle_text).size(20),
             text_input::<Message, Theme, Renderer>("Enter a card name...", &self.user_input).id("text-input").on_input(Message::InputChanged),
             button("Search").on_press(Message::Fetch),            
         ].into()
